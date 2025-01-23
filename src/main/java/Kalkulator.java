bc2390f33eb8f80276298dd0c0b2bb0c95ebc571
mrpythen <herman.nysted@gmail.com>
2025-01-23 19:43:49 +0800

    Laget metoder for plus minus og ganging

--- src/main/java/Kalkulator.java
+++ src/main/java/Kalkulator.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Kalkulator {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(plus(22,55));
+        System.out.println(minus(10,6));
+        System.out.println(gange(10,6));
+
+
+    }
+    public static double plus (double tall1, double tall2){
+        return (tall1 + tall2);
+    }
+    public static double minus (double tall1, double tall2){
+        return (tall1 - tall2);
+    }
+    public static double gange (double tall1, double tall2){
+        double x = tall1;
+        double resultat = 0;
+        for(double i =0; i < x; i++){
+            resultat += tall2;
+        }
+        return resultat;
+    }
+    p
+
+
 }
