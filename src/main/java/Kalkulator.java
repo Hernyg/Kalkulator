45327ff8b3189a0db5b7711b7921041cb56fc009
mrpythen <herman.nysted@gmail.com>
2025-01-24 16:00:05 +0800

    Innført tekstlig brukergrensesnitt med feilhåndtering for feil input

--- src/main/java/Kalkulator.java
+++ src/main/java/Kalkulator.java
@@ -1,14 +1,72 @@
+import java.lang.annotation.ElementType;
 import java.util.Scanner;
 
 public class Kalkulator {
 
     public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("velkommen til terminal kalkulatoren, for å fortsette vennligst skriv 2 heltall");
 
+        Scanner scanner = new Scanner(System.in);
+        double num1 = 0;
+        double num2 = 0;
 
-        System.out.println(plus(22,55));
-        System.out.println(minus(10,6));
-        System.out.println(gange(10,6));
+        boolean tall1Loop = true;
+        boolean tall2Loop = true;
+        boolean gyldigInput = true;
 
+        while(tall2Loop == true) {
+            while (tall1Loop == true) {
+
+                try {
+                    String inputet = scanner.nextLine();
+                    num1 = Double.parseDouble(inputet);
+                    System.out.println("neste nummer");
+                    tall1Loop = false;
+                } catch (NumberFormatException e) {
+                    System.out.println("FEIL skriv inn et gyldig nummer");
+                }
+            }
+
+            try {
+                String inputet = scanner.nextLine();
+                num2 = Double.parseDouble(inputet);
+                System.out.println("neste nummer");
+                tall2Loop = false;
+            }
+
+            catch (NumberFormatException e) {
+                System.out.println("FEIL skriv inn et gyldig nummer");
+            }
+        }
+            System.out.println("nummer en er " + num1 + "nummer 2 er " + num2);
+
+        System.out.println("For å fortsette vennligst velg ønsket regneopperasjon: +, - eller *");
+
+        while (gyldigInput == true){
+                String input = scanner.nextLine();
+                //System.out.println(input);
+
+                String regneopperasjon = input;
+                //bestemmer regneopperasjonen brukeren ønsker å gjennomføre
+                if (regneopperasjon.equals("+")){
+                    System.out.println("du har valgt +");
+                    System.out.println(num1 + " + " +  num2 + " = " + plus(num1,num2));
+                }
+                else if (regneopperasjon.equals("-")){
+                    System.out.println("du har valgt -");
+                    System.out.println(num1 + " - " +  num2 + " = " + minus(num1,num2));
+                }
+                else if (regneopperasjon.equals("*")){
+                    System.out.println("du har valgt *");
+                    System.out.println(num1 + " * " +  num2 + " = " + gange(num1,num2));
+                }
+                else{
+                    System.out.println("vennligst skriv inn en gyldig regne opperasjon");
+                }
+                break;
+
+            }
 
     }
     public static double plus (double tall1, double tall2){
@@ -25,7 +83,7 @@ public class Kalkulator {
         }
         return resultat;
     }
-    p
+
 
 
 }
